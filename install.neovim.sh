8697b9e6fd2d6799b8a8389ade9830434a367e36
honewatson <hone@hone.be>
2017-07-01 14:53:42 +1000

    Update build

--- install.neovim.sh
+++ install.neovim.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 sudo apt-get install python-dev python-pip python3-dev python3-pip
-
+sudo apt-get install build-essential cmake
+sudo apt-get install golang
+sudo apt-get install nodejs npm
+sudo npm install -g n yarn
+sudo n install stable
 # install conda
 sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
+#sudo apt-get update
+#sudo apt-get install neovim
 
 
-sudo pip install neovim
+#sudo pip install neovim
 # brew install neovim
 
 cd ~
-
 
 wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
 
 # Install Anaconda
 bash Anaconda3-4.3.0-Linux-x86_64.sh
 
+exit
+sudo conda install pip
+sudo pip install --upgrade pip
+sudo pip install setuptools
+sudo apt-get update 
+sudo apt-get install neovim
+sudo pip install neovim
 
+apt-get install ocaml aspcud darcs mercurial opam
 
+echo 'eval `opam config env`' > ~/.bashrc
+exit
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 cp ~/vim/init.vim ~/.config/nvim/init.vim
 
-nvim +PlugInstall +qall
-
 conda install libgcc
+
+nvim +PlugInstall +qall
 
 cd ~/.config/nvim/plugged/YouCompleteMe && git submodule update --init --recursive \
   && python install.py --gocode-completer --tern-completer
