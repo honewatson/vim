dbc6a88759b70321e9d16f9f438d4e8c73cb8a50
honewatson <hone@hone.be>
2019-08-07 19:15:07 +1000

    Update neovim

--- install.neovim.v2.sh
+++ install.neovim.v2.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
-sudo apt-get install python3-dev python3-pip
-sudo apt-get install build-essential cmake
-sudo apt-get install golang
-sudo apt-get install nodejs npm
-# TODO: Local Golang
-# TODO: Local Node NPM
-# TODO: 
+sudo apt install python3-dev python3-pip -y
+sudo apt install build-essential cmake -y
+sudo apt install golang -y
+sudo apt install nodejs npm -y
 sudo npm install -g n yarn
-sudo n install stable
-# install conda
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
+sudo n  stable
+install conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3*
+mkdir ~/bin
+echo "export PATH=~/.local/bin:~/.npm/bin:~/go/bin:~/bin:$PATH" >> ~/.bashrc-source
+echo "source ~/.bashrc-source" >> ~/.bashrc
+source ~/.bashrc-source
+cd ~/bin
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage nvim
+ln -s ~/bin/nvim ~/bin/vim
 curl https://sh.rustup.rs -sSf | sh
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc-source
 export PATH="$HOME/.cargo/bin:$PATH"
 cargo install ripgrep
+bash tmux-setup.sh
+sudo apt install fd-find -y
+sudo apt install neovim -y
+conda install pip
+pip install neovim
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-npm install -g typescript
-
-
+sudo npm install -g typescript
 cd ~
-sudo apt-get update 
-sudo apt-get install neovim
-
+ln -s ~/vim/basic/.vim/colors/monokai-phoenix.vim ~/.config/nvim/colors/monokai-phoenix.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-cp ~/vim/init.vim ~/.config/nvim/init.vim
-
+rsync -av ~/vim/basic/ ~/.config/nvim/
+ln -s ~/vim/basic/.vimrc ~/.config/nvim/init.vim
 nvim +PlugInstall +qall
-
 cd ~/.config/nvim/plugged/YouCompleteMe && git submodule update --init --recursive \
-  && python install.py --gocode-completer --rust-completer --js-completer
+  && python install.py --rust-completer
